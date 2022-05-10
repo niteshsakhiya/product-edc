0f1d13cb6a981ac0fd374b600613858c15d01afe
Brendan Cronin <brendan.cronin@mercedes-benz.com>
2022-05-10 11:56:39 +0200

    Clean vault entry Signed-off-by: Brendan Cronin <brendan.cronin@mercedes-benz.com>

--- edc-extensions/hashicorp-vault/src/main/java/org/eclipse/dataspaceconnector/core/security/hashicorp/HashicorpVaultEntry.java
+++ edc-extensions/hashicorp-vault/src/main/java/org/eclipse/dataspaceconnector/core/security/hashicorp/HashicorpVaultEntry.java
@@ -1,9 +1,12 @@
 package org.eclipse.dataspaceconnector.core.security.hashicorp;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+
 import java.util.Map;
 import java.util.Objects;
 
+@Data
 class HashicorpVaultEntry {
   private static final String INNER_DATA_KEY = "data";
 
@@ -34,79 +37,7 @@ class HashicorpVaultEntry {
   @JsonProperty("auth")
   private Map<String, Object> auth;
 
-  public String getLeaseId() {
-    return leaseId;
-  }
-
-  public void setLeaseId(String leaseId) {
-    this.leaseId = leaseId;
-  }
-
-  public String getRequestId() {
-    return requestId;
-  }
-
-  public void setRequestId(String requestId) {
-    this.requestId = requestId;
-  }
-
-  public Boolean getRenewable() {
-    return renewable;
-  }
-
-  public void setRenewable(Boolean renewable) {
-    this.renewable = renewable;
-  }
-
-  public Long getLeaseDuration() {
-    return leaseDuration;
-  }
-
-  public void setLeaseDuration(Long leaseDuration) {
-    this.leaseDuration = leaseDuration;
-  }
-
-  public Map<String, String> getMetadata() {
-    return metadata;
-  }
-
-  public void setMetadata(Map<String, String> metadata) {
-    this.metadata = metadata;
-  }
-
-  public Map<String, Map<String, String>> getData() {
-    return data;
-  }
-
   public Map<String, String> getInnerData() {
     return Objects.requireNonNull(data.get(INNER_DATA_KEY));
-  }
-
-  public void setData(Map<String, Map<String, String>> data) {
-    this.data = data;
-  }
-
-  public Map<String, Object> getWrapInfo() {
-    return wrapInfo;
-  }
-
-  public void setWrapInfo(Map<String, Object> wrapInfo) {
-    this.wrapInfo = wrapInfo;
-  }
-
-  public Map<String, Object> getWarnings() {
-    return warnings;
-  }
-
-  public void setWarnings(Map<String, Object> warnings) {
-    this.warnings = warnings;
-  }
-
-  public Map<String, Object> getAuth() {
-    return auth;
-  }
-
-  public void setAuth(Map<String, Object> auth) {
-    this.auth = auth;
   }
 }
